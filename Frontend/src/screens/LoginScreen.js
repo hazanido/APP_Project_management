5e2da0a9c4df86371f143266b5cbb09db81bdea5
hazanido <hazanido@gmail.com>
2024-11-02 17:48:29 +0200

    test

--- Frontend/src/screens/LoginScreen.js
+++ Frontend/src/screens/LoginScreen.js
@@ -1,16 +1,47 @@
-import React, { useState,useEffect  } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, TextInput, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as Google from 'expo-auth-session/providers/google';
-import { googleLogin } from '../api/googleAPI'; 
 import axios from '../api/backendAPI';
 import firebase from '../../firebaseConfig';
-
 
 const LoginScreen = ({ navigation }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    androidClientId: '485232347458-98sovcd93k1s4qd599ppi5d6ohoe85tt.apps.googleusercontent.com', // החלף ב-Client ID שקיבלת
+    redirectUri: 'http://localhost:8081', // ודא שה-redirect URI נכון
+  });
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      handleGoogleLogin(response.authentication);
+    }
+  }, [response]);
+
+  const handleGoogleLogin = async (authentication) => {
+    try {
+      if (authentication) {
+        const idToken = authentication.idToken;
+        const credential = firebase.auth.GoogleAuthProvider.credential(idToken);
+        const userCredential = await firebase.auth().signInWithCredential(credential);
+        const user = userCredential.user;
+
+        await AsyncStorage.setItem('userToken', idToken);
+        await AsyncStorage.setItem('userId', user.uid);
+
+        console.log('User logged in with Google:', user);
+        navigation.navigate('ProjectListScreen');
+      } else {
+        setErrorMessage('התחברות עם Google נכשלה.');
+      }
+    } catch (error) {
+      console.error("Error during Google login:", error);
+      setErrorMessage('שגיאה בהתחברות עם Google.');
+    }
+  };
 
   const handleLogin = async () => {
     try {
@@ -24,7 +55,6 @@ const LoginScreen = ({ navigation }) => {
       setErrorMessage('התחברות נכשלה. בדוק את פרטי הכניסה.');
     }
   };
-
 
   return (
     <View style={styles.container}>
@@ -55,7 +85,7 @@ const LoginScreen = ({ navigation }) => {
         <Image source={require('../../assets/Register_he.png')} style={styles.buttonImage} />
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={handleGoogleLogin}>
+      <TouchableOpacity onPress={() => promptAsync()}>
         <Image source={require('../../assets/google login_he.png')} style={styles.buttonImage} />
       </TouchableOpacity>
     </View>
